6e2e5dbae399e2d6a86235c02d45f0b7f3110d00
Tab Fitts <tfitts@spryservers.net>
2021-06-10 11:45:46 -0700

    Add IP and rDNS list generation functions

--- rdns.sh
+++ rdns.sh
@@ -1,1 +1,35 @@
 #!/bin/bash
+set -e
+
+source pdns.conf
+
+rm -f ip-arpa.txt ip-rdns.txt iplist.txt
+
+# Define some functions
+gen_ip_list() {
+  nmap -n -sL $RDNS_IP_SUBNET | awk '/Nmap scan report/{print $NF}' > iplist.txt
+}
+
+gen_ip_arpa() {
+  iplist="iplist.txt"
+  ips=$(cat $iplist)
+  for ip in $ips
+  do
+    echo "$ip" | awk -F . '{print $4"."$3"."$2"."$1".in-addr.arpa."}' >> ip-arpa.txt
+  done
+}
+
+gen_ip_rdns() {
+  iplist="iplist.txt"
+  ips=$(cat $iplist)
+  for ip in $ips
+  do
+    echo "$ip" | awk -F . '{print $4"-"$3"-"$2"-"$1".""'"$RDNS_DOMAIN"'"}' >> ip-rdns.txt
+  done
+}
+
+$(gen_ip_list)
+
+$(gen_ip_arpa)
+
+$(gen_ip_rdns)
