545131112cc2c08a192f104dc0cb3412c5a4bece
Tab Fitts <tfitts@spryservers.net>
2021-06-11 15:37:11 -0700

    Create folder for json payloads. Use a tmp directory.
    Create a folder for json payploads and move existing json
    template into the directory.
    
    Begin using a tmp dir for var output files and payloads.
    Cleanup tmp dir after running apply-rdns-template script.

--- apply-rdns-template.sh
+++ apply-rdns-template.sh
@@ -10,38 +10,38 @@
 # Mass Update rDNS
 #
 ################################################################
-set -e
+
 
 ########### Import config vars ###########
 source pdns.conf
 
-### Cleanup any previous leftover files ###
-rm -f ip-list.txt ip-var.txt curlPayloadPTRrecord.json
+###### Create Temporary Directory ########
 
+mkdir tmp
 ########## Define some functions ##########
 gen_ip_list() {
-  nmap -n -sL $RDNS_IP_SUBNET | awk '/Nmap scan report/{print $NF}' > ip-list.txt
+  nmap -n -sL $RDNS_IP_SUBNET | awk '/Nmap scan report/{print $NF}' > tmp/ip-list.txt
 }
 
 pdns_payload_generate() {
-  cat addPTR.template.json | sed -e 's|ip_arpa|'"$ip_arpa"'|g' -e 's|rdns_entry|'"$rdns_entry"'|g' > curlPayloadPTRrecord.json
+  cat payload-templates/addPTR.template.json | sed -e 's|ip_arpa|'"$ip_arpa"'|g' -e 's|rdns_entry|'"$rdns_entry"'|g' > tmp/curlPayloadPTRrecord.json
 }
 
 pdns_curl() {
   curl -H "X-API-Key: $PDNS_API_KEY" \
        -H "Content-Type: application/json" \
-       -d @curlPayloadPTRrecord.json \
+       -d @tmp/curlPayloadPTRrecord.json \
        -X PATCH $PDNS_API_URL/api/v1/servers/localhost/zones/$PDNS_ZONE_ID
 }
 
 push_payload() {
-  iplist="ip-list.txt"
+  iplist="tmp/ip-list.txt"
   ips=$(cat $iplist)
   for ip in $ips
   do
-    echo "$ip" | awk -F . '{print "ip_arpa="""$4"."$3"."$2"."$1".in-addr.arpa."""}' > ip-var.txt
-    echo "$ip" | awk -F . '{print "rdns_entry="""$4"-"$3"-"$2"-"$1".""'"$RDNS_DOMAIN"'"""}' >> ip-var.txt
-    source ip-var.txt
+    echo "$ip" | awk -F . '{print "ip_arpa="""$4"."$3"."$2"."$1".in-addr.arpa."""}' > tmp/ip-var.txt
+    echo "$ip" | awk -F . '{print "rdns_entry="""$4"-"$3"-"$2"-"$1".""'"$RDNS_DOMAIN"'"""}' >> tmp/ip-var.txt
+    source tmp/ip-var.txt
     $(pdns_payload_generate)
     $(pdns_curl)
   done
@@ -51,3 +51,6 @@ push_payload() {
 $(gen_ip_list)
 ############## Push Payload ###############
 $(push_payload)
+
+###### Remove Temporary Directory #########
+rm -rf tmp
